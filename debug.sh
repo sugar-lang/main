958fbd07c7a07e025a12e629a2b0af38867693ad
Sebastian Erdweg <seba.github@gmail.com>
2016-04-05 17:45:24 +0200

    modified debug script

---
+++ debug.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# from https://raw.githubusercontent.com/fniephaus/travis-remote-shell/master/run.sh
+
+wget -O ngrok.zip https://dl.ngrok.com/ngrok_2.0.19_linux_amd64.zip
+unzip ngrok.zip
+{
+    nc -l -v 8888
+    killall -SIGINT ngrok && echo "ngrok terminated"
+} &
+{
+    ./ngrok tcp 8888 --authtoken=$NGROK_TOKEN --log=stdout --log-level=debug | grep "tcp.ngrok.io" || true
+} &
